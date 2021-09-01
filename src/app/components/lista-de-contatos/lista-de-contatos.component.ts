0d703a8f205e6054b8b480dff61d0428ffb75fb7
vpmys <victorpaivayao@gmail.com>
2021-09-01 20:56:02 -0300

    Adicionado contato a lista de contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome:"Victor Yao 1",
+      email:"yao@teste.com",
+      telefone:['1234 - 4321']
+    },
+    {
+      nome:"Victor Yao 2",
+      email:"yao@teste.com",
+      telefone:['2234 - 4322']
+    },
+    {
+      nome:"Victor Yao 3",
+      email:"yao@teste.com",
+      telefone:['3234 - 4323']
+    },
+    {
+      nome:"Victor Yao 4",
+      email:"yao@teste.com",
+      telefone:['4234 - 4324']
+    },
+    {
+      nome:"Victor Yao 5",
+      email:"yao@teste.com",
+      telefone:['5234 - 4325']
+    },
+    {
+      nome:"Victor Yao 6",
+      email:"yao@teste.com",
+      telefone:['6234 - 4326']
+    },
+    {
+      nome:"Victor Yao 7",
+      email:"yao@teste.com",
+      telefone:['7234 - 4327']
+    },
+    {
+      nome:"Victor Yao 8",
+      email:"yao@teste.com",
+      telefone:['8234 - 4328']
+    }
+  ]
 
   constructor() { }
 
